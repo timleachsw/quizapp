208e49a3c354fad01e4e66289aca8fec1159ad3b
timleachsw <Tim.Leach@softwire.com>
2020-02-11 15:32:31 +0000

    Implemented javascript username input

---
+++ v0.3/username.js
@@ -0,0 +1,10 @@
+function setUsername() {
+    const usernameInput = document.getElementsByClassName("username-input")[0];
+    const username = usernameInput.value;
+
+    // Replace form with a display of the username, but only if they've entered something
+    if (username) {
+        document.getElementsByClassName("username-set")[0].innerHTML = `
+        <h2>User ${username}</h2>`;
+    }
+}
